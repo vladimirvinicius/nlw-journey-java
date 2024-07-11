d13b209f4fa269169776e180a6ac485c524aef34
Vladimir Vinicius <vladimirvinicius@gmail.com>
2024-07-11 14:02:08 -0300

    initial commit

---
+++ src/main/java/com/rocketseat/planner/activity/ActivityService.java
@@ -0,0 +1,30 @@
+package com.rocketseat.planner.activity;
+
+import com.rocketseat.planner.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ActivityService {
+
+    @Autowired
+    private  ActivityRepository repository;
+
+    public ActivityResponse registerActivity(ActivityRequestPayload payload, Trip trip){
+
+        Activity newActivity = new Activity(payload.title(), payload.occursAt(), trip);
+
+        this.repository.save(newActivity);
+
+        return new ActivityResponse(newActivity.getId());
+
+    }
+
+    public List<ActivityData> getAllActivitiesFromId(UUID tripId){
+        return this.repository.findTripById(tripId).stream().map(activity -> new ActivityData(activity.getId(), activity.getTitle(), activity.getOccursAt())).toList();
+
+    }
+}
